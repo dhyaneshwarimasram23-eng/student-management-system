4b58f0fb5f1248759b2bb2f6c27eb7e3928c621d
Dhy_m22 <dhyaneshwari.masram23@spit.ac.in>
2026-03-02 19:30:17 +0530

    Refactor function

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
-function login(user) {
+const login = (user) => {
+    switch (user.role) {
+        case "student":
+            return "Student Dashboard";
+        default:
+            return "Unauthorized";
+    }
+};
+
+module.exports = login;function login(user) {
     if (user.role === "student") {
         return "Welcome Student";
     }
